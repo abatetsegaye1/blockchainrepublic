5994708ff6be330257a31a1dc9adc04de8ed8ea9
unknown <abatetsegaye12@gmail.com>
2025-02-07 23:36:04 +0300

    added message on

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.post("createUser",  async (req, res) => {
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 
   })
   .then(() => {
