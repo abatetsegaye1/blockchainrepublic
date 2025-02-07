30a3ba7745ce0d881e417d55e2fc78df5edcdabc
unknown <abatetsegaye12@gmail.com>
2025-02-07 23:39:25 +0300

    commit two

--- server.js
+++ server.js
@@ -47,13 +47,13 @@ app.post("createUser",  async (req, res) => {
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
 
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
+    app.listen(4000, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
